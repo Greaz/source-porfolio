78a3a438a96dbce44d100d3c37a74283b4ddffff
Rémy Saddour <rsaddour@auchandirect.net>
2017-12-23 13:27:03 +0100

    add globale style

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -5,8 +5,8 @@ export class Header extends React.Component {
   render() {
     const { text } = this.props;
     return (
-      <div>
-          <h1> {text} </h1>
+      <div class="header">
+          <h1>{ text }</h1>
       </div>
     );
   }
