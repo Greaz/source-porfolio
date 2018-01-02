af5db54cc283525135e014b75046283286ec2a76
Rémy Saddour <rsaddour@auchandirect.net>
2018-01-02 20:51:42 +0100

    create previewCase component

---
+++ app/components/previewcase.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+import Auchan from "../img/svg/adi.svg"
+import Carrefour from "../img/svg/carrefour.svg"
+import Planet from "../img/svg/planet.svg"
+import Groupama from "../img/svg/groupama.svg"
+import Default from "../img/svg/default.svg"
+
+export class PreviewCase extends React.Component {
+  
+  renderImage(img) {
+    if (img === 1) {
+      return <img src={ Auchan } alt="Auchan preview case" />;
+    }
+    else if (img === 2) {
+      return <img src={ Carrefour } alt="Carrfour preview case" />;
+    }
+    else if (img === 3) {
+      return <img src={ Planet } alt="PlanetVO preview case" />;
+    }
+    else if (img === 4) {
+      return <img src={ Groupama } alt="Groupama preview case" />;
+    }
+    else {
+      return <img src={ Default } alt="Groupama preview case" />;
+    }
+  }
+
+  render() {
+    const { 
+      titre,
+      sousTitre,
+      titreJob,
+      img,
+     } = this.props;
+    return (
+      <div className="ref">
+        <h3> { titre } </h3>
+        <span> { titreJob } </span>
+        <span> { sousTitre } </span>
+        { this.renderImage(img) }
+      </div>
+    );
+  }
+}
+module.exports = PreviewCase;
--- app/homepage.jsx
+++ app/homepage.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "components/header.jsx"
+import PreviewCase from "components/previewcase.jsx"
 
 export class Homepage extends React.Component {
   render() {
@@ -10,8 +11,30 @@ export class Homepage extends React.Component {
               Hello, i solve <span>design problems</span> by designing <span>suitable experiences</span> for <span>the web</span> and <span>the mobile</span>.
             </h1>
           </Header>
-          <h2> zone de cases studies H2 </h2>
-          <h3> zone réfs H3 </h3>
+          <PreviewCase 
+            titre= "Auchan:Direct"
+            titreJob= "« CREATIVE TECHNOLOGIST »"
+            sousTitre= "UX, UI, PROTOTYPES, INTERACTIONS, MOTION DESIGN"
+            img= { 1 }
+          />
+          <PreviewCase 
+            titre= "Carrfour Drive"
+            titreJob= "« UI/UX DESIGNER »"
+            sousTitre= "UX, UI, MICRO ANIMATIONS"
+            img= { 2 }
+          />
+          <PreviewCase 
+            titre= "Planet VO2"
+            titreJob= "« INTERACTION DESIGNER »"
+            sousTitre= "INTERACTIONS, PROTOTYPES, MOTION DESIGN"
+            img= { 3 }
+          />
+          <PreviewCase 
+            titre= "Groupama (studiant quotes)"
+            titreJob= "« PROTOTYPER »"
+            sousTitre= "UI, INTERACTIONS, PROTOTYPES"
+            img= { 4 }
+          />
       </div>
     );
   }
