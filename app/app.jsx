e4c9c228f6da9610e243255aecf676e4d78a3214
Rémy Saddour <rsaddour@auchandirect.net>
2018-01-10 11:39:14 +0100

    add react-helmet to package.json and add meta viewport to app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -1,11 +1,17 @@
 import React from "react"
 import Navigation from "components/navigation.jsx"
 import Routes from "routes.jsx"
+import Helmet from "react-helmet";
 
 export class App extends React.Component {
   render() {
     return (
       <div>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, user-scalable=no" />
+          <title>Portfolio Rémy Saddour</title>
+        </Helmet>
         <Navigation/>
         <Routes/>
       </div>
