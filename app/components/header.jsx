b7ff3cfcf4fb74332e96d64437a47258aab0add1
Rémy Saddour <rsaddour@auchandirect.net>
2018-01-11 17:01:21 +0100

    modify cases studies images

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -4,9 +4,12 @@ import Image from "components/image.jsx"
 export class Header extends React.Component {
   
   render() {
-    const { children } = this.props;
+    const { 
+      children,
+      gradiant,
+    } = this.props;
     return (
-      <div className="header">
+      <div className="header" style={ gradiant }>
           <div>{ children }</div>
           <Image img={5} />
       </div>
