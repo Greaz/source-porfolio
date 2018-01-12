2a953c9a387a26247685d0177d0f5e373b9e6605
Rémy Saddour <rsaddour@auchandirect.net>
2018-01-12 17:04:21 +0100

    add a component for reseting scroll on navigation

--- app/index.js
+++ app/index.js
@@ -1,13 +1,16 @@
 import "index.scss"
 import React from "react"
 import ReactDOM from "react-dom"
-import { HashRouter } from 'react-router-dom'
+import { HashRouter } from "react-router-dom"
+import ScrollToTop from "components/scrolltotop.jsx"
 
 import App from "app.jsx"
 
 ReactDOM.render(
   <HashRouter>
-    <App/>
+    <ScrollToTop>
+      <App/>
+    </ScrollToTop>
   </HashRouter>,
     document.body
 );
