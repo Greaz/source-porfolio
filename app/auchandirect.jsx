bb0d1f190889ef6af09849b63db2e35ad82439e9
Rémy Saddour <rsaddour@auchandirect.net>
2018-01-31 11:38:38 +0100

    add label header in auchandirect

--- app/auchandirect.jsx
+++ app/auchandirect.jsx
@@ -16,7 +16,8 @@ export class Auchandirect extends React.Component {
         <div>
           <Header>
             <h1>
-              <span>Stop Bullshit, start good shit.</span><br /> Allow users to do their food shopping of the month in less than 20 minutes.
+              <p>Mission objectif</p>
+              Allow users to do their <span>food shopping</span> of the month in <span>less than 20 minutes.</span>
             </h1>
           </Header>
           <section className="title-case">
